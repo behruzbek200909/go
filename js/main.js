ba53a97795c6c9973b0278688545756bbf422dec
behruzbek200909 <behruzbekmahkamov94@gmail.com>
2024-05-25 19:21:06 +0600

    go

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//// var = variable
+// var ism = "Behruzbek"
+// let ism = "BEHRUZBEK"
+// const ism = "behruzbek"
+
+// console.log(ism);
+
+
+
+
+// var a = "salom"
+// a = "alik"
+// var b = "a"
+// console.log(a);
+
+// const a = "salom"
+// a = "alik"
+
+// console.log(a);
+
+// let ismFamiliya // CamelCase
+// let ism__familiya // snakeCase
+// let IsmFamiliya // PascalCase
+// let ism-familiya // KebabCase
+
+let a = 10 
+let b = 20
+let c = 30
+let d = 40
+let e = 50
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+let name = "Behruzbek"
+let transport = "car"
+let city = "park"
+let livingroom = "plant"
+let hallowen = "ghost"
+console.log(name);
+console.log(transport);
+console.log(city);
+console.log(livingroom);
+console.log(hallowen);
+
+let isMerred = true
+let Itrains = no    
+let thestreetiscold = true
+let thewarstarted = no
+console.log(typeof isMerred);
+console.log(Itrains);
+console.log(thestreetiscold);
+console.log(thewarstarted);
+
